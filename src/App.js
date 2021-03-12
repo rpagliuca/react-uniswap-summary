8de0862514f73ed10980d00251f987b891e83ab1
Rafael Santos <rafael.santos.193@gmail.com>
2021-03-12 02:36:19 -0300

    feat: New card template

--- src/App.js
+++ src/App.js
@@ -19,8 +19,36 @@ function Main() {
     document.cookie = "endpoint=" + endpoint + "; SameSite=Strict; Max-Age=" + oneYear;
     setEndpoint(endpoint)
   }
+  
+  let hashEndpoint = window.location.hash.substr(1);
 
-  useEffect(() => {
+  const activateHashChangeHandler = () => {
+
+    if (!("onhashchange" in window)) {
+      return () => {};
+    }
+
+    const handler = () => {
+      hashEndpoint = window.location.hash.substr(1);
+      loadHashFromURL();
+    }
+
+    window.onhashchange = handler;
+
+    // Callback to deactivate the current handler
+    return () => {
+      window.onhashchange = null;
+    }
+
+  };
+
+  const loadHashFromURL = () => {
+    if (hashEndpoint) {
+      persistEndpoint(hashEndpoint)
+    }
+  };
+
+  const loadHashFromCookie = () => {
     let cookieValue = "";
     try {
       cookieValue = document.cookie
@@ -28,10 +56,27 @@ function Main() {
         .find(row => row.startsWith('endpoint='))
         .split('=')[1];
     } catch {}
+
     if (cookieValue) {
       setEndpoint(cookieValue);
     }
-  }, [endpoint]);
+  }
+
+  useEffect(() => {
+
+    // Load from URL
+    loadHashFromURL();
+
+    // Load from cookie
+    loadHashFromCookie();
+
+    const deactivateHashChangeHandler = activateHashChangeHandler()
+
+    return () => {
+      deactivateHashChangeHandler()
+    }
+
+  });
 
   if (!endpoint) {
     return <Setup setEndpoint={persistEndpoint}/>;
@@ -73,14 +118,11 @@ function Stats({handleClear, endpoint}) {
   const [data, setData] = useState({"isLoading": true, "shouldLoad": true});
 
   useEffect(() => {
-    if (data.shouldLoad) {
-      data.shouldLoad = false;
-      setData(data);
-      fetch(endpoint)
-        .then(response => response.json().then(data => setData(data)))
-        .catch((error) => setData({"error": error.toString()}))
-    }
-  }, [endpoint, data]);
+    setData({"isLoading": true});
+    fetch(endpoint)
+      .then(response => response.json().then(data => setData(data)))
+      .catch((error) => setData({"error": error.toString()}))
+  }, [endpoint]);
 
   if (data.isLoading) {
     return (
@@ -97,70 +139,107 @@ function Stats({handleClear, endpoint}) {
   let idx = 0;
 
   return (
-    <rs.Container>
-      <rs.Row>
+
+    <rs.Row>
 
         {data.map(i => {
 
           idx += 1;
 
+          const positionType = i.Token.Token1InitialQuantity < 0 ? "position-negative" : "position-positive";
+
           return (
 
-          <rs.Col key={idx}>
+            <rs.Col md="6">
 
-            <rs.Card>
+            <rs.Card key={idx} className={positionType}>
               <rs.CardBody>
 
-                <rs.CardTitle tag="h5">{i.Token.Pair.Id}</rs.CardTitle>
+                <rs.CardTitle tag="h5"><rs.Badge color="warning">{i.Token.Pair.Id}</rs.Badge></rs.CardTitle>
 
-                <rs.Row><rs.Col>
-                    <b>{i.Token.Token1.Id}:</b> {i.Token1FinalQuantity.toFixed(3)}
-                </rs.Col></rs.Row>
+                <rs.Badge color="dark">Initial position @ {i.Token.InitialDate.replace("T", " ").replace("Z", " UTC")}</rs.Badge>
 
-                <rs.Row><rs.Col>
-                    <b>{i.Token.Token2.Id}:</b> {i.Token2FinalQuantity.toFixed(3)}
-                </rs.Col></rs.Row>
+                <rs.Card className="internal-card">
+                  <rs.CardBody>
 
-                <rs.Row><rs.Col>
-                    <b>K:</b> {i.MyK.toFixed(3)}
-                </rs.Col></rs.Row>
+                    <rs.Row><rs.Col>
+                        <b>{i.Token.Token1.Id}:</b> {i.Token.Token1InitialQuantity.toFixed(3)}
+                    </rs.Col></rs.Row>
 
-                <rs.Row><rs.Col>
-                    <b>Initial price:</b> {i.InitialPrice.toFixed(3)} {i.Token.Token1.Id}/{i.Token.Token2.Id}
-                </rs.Col></rs.Row>
+                    <rs.Row><rs.Col>
+                        <b>{i.Token.Token2.Id}:</b> {i.Token.Token2InitialQuantity.toFixed(3)}
+                    </rs.Col></rs.Row>
 
-                <rs.Row><rs.Col>
-                    <b>End price:</b> {i.FinalPrice.toFixed(3)}
-                </rs.Col></rs.Row>
+                    <rs.Row><rs.Col>
+                        <b>K:</b> {i.InitialK.toFixed(3)}
+                    </rs.Col></rs.Row>
 
-                <rs.Row><rs.Col>
-                    <b>Divergence loss (%):</b> {i.DivergenceLoss.toFixed(2)}
-                </rs.Col></rs.Row>
+                    <rs.Row><rs.Col>
+                        <b>Initial price:</b> {i.InitialPrice.toFixed(3)} {i.Token.Token1.Id}/{i.Token.Token2.Id}
+                    </rs.Col></rs.Row>
 
-                <rs.Row><rs.Col>
-                    <b>Fees (%):</b> {i.PercentageFees.toFixed(2)}
-                </rs.Col></rs.Row>
+                  </rs.CardBody>
+                </rs.Card>
 
-                <rs.Row><rs.Col>
-                    <b>Accrued profit (%):</b> {i.AccruedProfit.toFixed(2)}
-                </rs.Col></rs.Row>
+                <rs.Badge color="dark">Current position</rs.Badge>
 
-                <rs.Row><rs.Col>
-                    <b>Yearly profit rate (%):</b> {i.YearlyProfit.toFixed(2)}
-                </rs.Col></rs.Row>
+                <rs.Card className="internal-card">
+                  <rs.CardBody>
+
+                    <rs.Row><rs.Col>
+                        <b>{i.Token.Token1.Id}:</b> {i.Token1FinalQuantity.toFixed(3)}
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>{i.Token.Token2.Id}:</b> {i.Token2FinalQuantity.toFixed(3)}
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>K:</b> {i.MyK.toFixed(3)}
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>End price:</b> {i.FinalPrice.toFixed(3)} {i.Token.Token1.Id}/{i.Token.Token2.Id}
+                    </rs.Col></rs.Row>
+
+                  </rs.CardBody>
+
+                </rs.Card>
+
+                <rs.Badge color="dark">Results</rs.Badge>
+                <rs.Card className="internal-card">
+                  <rs.CardBody>
+                    <rs.Row><rs.Col>
+                        <b>Divergence loss:</b> {i.DivergenceLoss.toFixed(2)}%
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>Fees:</b> {i.PercentageFees.toFixed(2)}%
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>Current profit:</b> {i.AccruedProfit.toFixed(2)}%
+                    </rs.Col></rs.Row>
+
+                    <rs.Row><rs.Col>
+                        <b>Yearly profit rate:</b> {i.YearlyProfit.toFixed(2)}%
+                    </rs.Col></rs.Row>
+
+                  </rs.CardBody>
+                </rs.Card>
+
+
+
 
 
 
               </rs.CardBody>
             </rs.Card>
+            </rs.Col>
 
-          </rs.Col>
         )})}
-        
 
-      </rs.Row>
-
-    </rs.Container>
+    </rs.Row>
   );
 }
 
